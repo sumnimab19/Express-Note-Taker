7d1e64318cebf9509238a5e8d921fd7f4d5921ff
Sumnima B <sumnimabhandari@gmail.com>
2020-06-16 21:12:22 -0700

    Fixed js and css link on index.html and notes.html files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,20 @@
 
 const express = require('express');
 
-const notesData = require("../db/db");
+const path = require("path");
+
+const fs = require("fs");
+
+const notesData = require("../db/db.json");
 
 const router = express.Router();
 
-router.get("/notes", function (req, res) {
+router.get("/api/notes", function (req, res) {
   res.json(notesData);
+  // res.sendFile(path.join(__dirname, "../db/db.json"))
 });
 
-router.post("/api/notes", function (req, res) {
-  notesData.push(req.body);
-});
 
-router.delete("/api/notes/:id", function (req, res) {
-  res.json(notesData.filter(notesData => notesData.id === parseInt(req.params.id)))
-
-});
 
 
 module.exports = router;
